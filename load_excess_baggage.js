6cc8f6f3b31436d48d6592ccdd0206213cd5452a
analytics-scripts <54020175+analytics-scripts@users.noreply.github.com>
2020-02-11 11:52:26 +0300

    Create load_excess_baggage.js

---
+++ load_excess_baggage.js
@@ -0,0 +1,37 @@
+(function(){
+    var success = false;
+
+    for (var i = 0; i < 1; ++i) {
+        if (success) {
+            break;
+        }
+
+        (async () => {
+            const rawResponse = await fetch('https://www.aeroflot.ru/sb/booking/api/app/search_excess_baggage/v1', {
+              method: 'POST',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'cookie': document.cookie
+              },
+              body: '{"pnr_locator":"' + document.location.hash.split("pnr_locator=")[1].replace(/&.*/g, "") + '","pnr_key":"' + document.location.hash.split("pnr_key=")[1].replace(/&.*/g, "") + '","lang":"' + document.cookie.split("AF_preferredLanguage=")[1].replace(/;.*/, "").toLowerCase() + '","pos_country":"' + document.cookie.split("POS_COUNTRY=")[1].replace(/;.*/, "").toLowerCase() + '"}'
+            });
+
+            const content = await rawResponse.json();
+
+            if (content.success) {
+                success = true;
+            }
+
+            if (content && content.data && content.data.available) {
+                dataLayerSU.push({
+                    "event": "AeroinformEvent_78_2",
+                    "eventCategory": "ab_test",
+                    "eventAction": "load_orgigin_service",
+                    "eventLabel": "excess_baggage",
+                    "customdimension5": "B"
+                });
+            }
+        })();
+    }
+})();
